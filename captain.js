44a16ce248e64d0eef091123ae0c4770a7465be1
anakiemai <andrewatisl@gmail.com>
2025-10-27 12:34:43 +0000

    Delete captain.js

--- captain.js
+++
@@ -1,6 +0,0 @@
-// rooms/captain.js
-game.rooms.captain = {
-  desc: "You enter the Captain's (sadly, quite small and a bit smelly) office. You're stood in front of the Captain.",
-  exits: { north: "bridge" },
-  items: []
-};
